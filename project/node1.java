0c63c302b0ee92f3345e7053608a4066e1cba331
nawaaf138 <54602312+nawaaf138@users.noreply.github.com>
2021-07-01 22:22:18 +0300

    Add files via upload

---
+++ project/node1.java
@@ -0,0 +1,35 @@
+package project;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class node1 {
+	static WebDriver driver;
+	static String NodeURL;
+	@Test
+	public void f() 
+	{
+		try {
+			NodeURL = " http://192.168.43.223:4444/wd/hub ";
+			DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+			capabilities.setBrowserName("chrome");
+			capabilities.setPlatform(Platform.WINDOWS);
+			driver = new RemoteWebDriver(new URL(NodeURL), capabilities);
+			driver.manage().deleteAllCookies();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(45, TimeUnit.SECONDS);
+			driver.get("https://www.swiggy.com/");
+			driver.findElement(By.xpath("//*[@id=\"root\"]/div[1]/div[1]/div/div[1]/div[1]/div/div[1]/div/a[1]")).click();
+			driver.findElement(By.xpath("//*[@id=\"mobile\"]")).sendKeys("+966000000000");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
